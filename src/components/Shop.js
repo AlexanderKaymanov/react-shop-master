afe5ceec4f66197e9e0aabe76a10ed07a23b8221
Alexander Kaymanov <alserg1059@yandex.ru>
2021-08-29 19:59:58 +0400

    Изменены расширения файлов

---
+++ src/components/Shop.js
@@ -0,0 +1,11 @@
+// import { useState, useEffect } from 'react';
+// import { API_KEY, API_URL } from '../config';
+
+function Shop() {
+
+    return (
+        <main className='container content'>Shop</main>
+    );
+}
+
+export { Shop };
