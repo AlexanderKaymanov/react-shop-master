e21f493bc639c2ab99aee8858c5c1d449be21420
Alexander Kaymanov <alserg1059@yandex.ru>
2021-09-10 13:22:31 +0400

    gh-pages added

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
                   React Shop
               </a>
               <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="!#">Repo</a></li>
+            <li><a href="https://alexanderkaymanov.github.io/react-shop-master/" target="_blank" rel="noreferrer">Repo</a></li>
         </ul>
         </div>
     </nav>
